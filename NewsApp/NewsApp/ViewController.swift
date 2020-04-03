cb2a3897d01c8aa469518109aae7c2a8e9dd21fb
«Veronika <veronagonchar@gmail.com>
2020-04-03 19:00:17 +0300

    create a repozitory of new project

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Вероника on 4/3/20.
+//  Copyright © 2020 Veronika Gonchar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
